29300e25a66f5d0e2195a17c299274131e7ffed3
leeyanggoo <didrn94553@gmail.com>
2024-04-25 20:57:09 +0900

    feat: CSS Reset 설정

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { Reset } from 'styled-reset';
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <Reset />
+    <App />
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
