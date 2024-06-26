5730f1880860f54ffda72f67487c32d93f97caf6
이양구 <didrn94553@gmail.com>
2024-06-27 01:32:35 +0900

    feat: page Login/Signup

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,26 +1,112 @@
-import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { DevTool } from '@hookform/devtools';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+import { InputControl } from '@components/Input';
+import Button from '@components/Button';
+
+// TODO: 로그인에 얼마나 validation을 추가할지?
+const schema = z.object({
+  email: z.string().min(1, { message: '이메일을 입력해주세요.' }),
+  password: z.string().min(1, { message: '비밀번호를 입력해주세요.' }),
+});
+
+type Schema = z.infer<typeof schema>;
 
 const Login = () => {
   const navigate = useNavigate();
 
-  const login = () => {
-    localStorage.setItem('user', JSON.stringify(true));
-    navigate('/');
+  const {
+    control,
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting, isSubmitted, isDirty, isValid },
+    reset,
+  } = useForm<Schema>({
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+    mode: 'onSubmit',
+    resolver: zodResolver(schema),
+  });
+
+  const onSubmit: SubmitHandler<Schema> = data => {
+    alert(JSON.stringify(data));
+    // TODO
+    // try => navigate('/')
+    // catch => reset(data)
   };
 
   return (
-    <div>
-      <h1>로그인 페이지</h1>
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: '100px 0',
+        width: '350px',
+        margin: '0 auto',
+      }}
+    >
+      <h1
+        style={{
+          fontSize: '30px',
+          fontWeight: 'bold',
+          marginBottom: '60px',
+        }}
+      >
+        로그인
+      </h1>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        style={{
+          width: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+          gap: '20px',
+        }}
+        autoComplete='off'
+      >
+        <InputControl
+          $fullWidth
+          $label='이메일'
+          $inputProps={{
+            ...register('email'),
+            id: 'email',
+            autoComplete: 'email',
+          }}
+          $error={!!errors.email}
+          $helperText={errors.email?.message || undefined}
+        />
 
-      <h2>
-        <Link to={'/'}>메인으로 돌아가기</Link>
-      </h2>
+        <InputControl
+          $fullWidth
+          $label='비밀번호'
+          $inputProps={{
+            ...register('password'),
+            id: 'password',
+            type: 'password',
+            autoComplete: 'current-password',
+          }}
+          $error={!!errors.password}
+          $helperText={errors.password?.message || undefined}
+        />
 
-      <button
-        onClick={login}
-        style={{ background: 'blue', padding: '10px', color: 'white' }}
-      >{`로그인(loacalStorage에 user 만듦)`}</button>
+        <Button
+          type='submit'
+          disabled={!isDirty || !isValid || isSubmitting}
+          $size='large'
+        >
+          로그인
+        </Button>
+      </form>
+      <DevTool control={control} />
     </div>
   );
 };
--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -1,38 +1,10 @@
-import Button from '@components/Button';
-import Container from '@components/Container';
-import Input from '@components/Input';
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 const Main = () => {
-  const user = localStorage.getItem('user');
   return (
-    <Container>
-      <h1>
-        메인 페이지 -{' '}
-        {user ? (
-          <span style={{ color: 'green' }}>로그인했습니다</span>
-        ) : (
-          <span style={{ color: 'red' }}>로그인 안 되어 있음</span>
-        )}
-      </h1>
-
-      <Button>아하</Button>
-
-      <Input />
-
-      <h2>
-        <Link to={'/signup'}>회원가입 페이지</Link>
-      </h2>
-
-      <h2>
-        <Link to={'/login'}>로그인 페이지</Link>
-      </h2>
-
-      <h2>
-        <Link to={'/mypage'}>마이 페이지</Link>
-      </h2>
-    </Container>
+    <>
+      <h1>메인페이지</h1>
+    </>
   );
 };
 
